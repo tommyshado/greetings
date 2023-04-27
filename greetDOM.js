752ee69238258e2250341e91a60f1e4d50f396e8
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-27 21:39:54 +0200

    got reference using document.querySelector selecting the class '.clearBtn'

--- greetDOM.js
+++ greetDOM.js
@@ -1,5 +1,6 @@
 const nameElementRef = document.querySelector('.nameString');
 const greetBtnRef = document.querySelector('.greetMeBtn');
+const clearBtnRef = document.querySelector('.clearBtn');
 const greetingRef = document.querySelector('.displayGreet');
 
 greetBtnRef.addEventListener('click', () => {});
