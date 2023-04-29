08a1ec1e5e2244305b3d261ff4a3dea0d0253f4d
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-29 08:37:44 +0200

    removed the else condition, also removed the || errorString and var errorString

--- greet.js
+++ greet.js
@@ -1,18 +1,14 @@
 const greetFactory = () => {
     let nameString = '';
-    let errorString = '';
 
     const getName = function(name) {
         if(typeof name === 'string') {
             nameString = `${(name.toLowerCase()).trim()}`;
-        } else {
-            errorString = 'Invalid input';
         }
     } 
 
     const logsName = function() {
-        return nameString || errorString;
+        return nameString;
     }
-
     return { getName, logsName };
 }
