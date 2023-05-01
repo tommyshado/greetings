96d97179e09faf37411c922302c52c824402fe38
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-01 15:38:24 +0200

    got the reference to the radio buttons

--- greetDOM.js
+++ greetDOM.js
@@ -2,6 +2,7 @@ const nameElementRef = document.querySelector('.nameString');
 const greetBtnRef = document.querySelector('.greetMeBtn');
 const greetingRef = document.querySelector('#greet');
 const counterGreet = document.querySelector('#counter');
+const radioButtonRef = document.querySelector('.language');
 
 let instaOfGreetFactory = greetFactory();
 
