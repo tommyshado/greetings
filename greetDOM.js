4159fa88889409ad0ca84d4d8692ce99c9eda9a9
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-04 14:14:26 +0200

    initialized counter key into the localStorage

--- greetDOM.js
+++ greetDOM.js
@@ -2,8 +2,12 @@ const nameElementRef = document.querySelector('.nameString');
 const greetBtnRef = document.querySelector('.greetMeBtn');
 const inputGreetRef = document.querySelector('#greet');
 const counterGreet = document.querySelector('#counter');
-const radioButtonRef = document.querySelector('.language');
+const radioButtonRef = document.querySelectorAll('.language');
 const resetBtn = document.querySelector('.resetBtn');
+
+if (localStorage['counter']) {
+    counterGreet.innerHTML = Number(localStorage['counter']);
+}
 
 const instaOfGreetFactory = greetFactory();
 
@@ -21,10 +25,15 @@ greetBtnRef.addEventListener('click', () => {
     nameElementRef.value = '';
 
     instaOfGreetFactory.logsName();
-    counterGreet.innerHTML = instaOfGreetFactory.logsCounter();
+    counterGreet.innerHTML = instaOfGreetFactory.greetCounter();
+    localStorage['counter'] = instaOfGreetFactory.greetCounter();
 });
 
 resetBtn.addEventListener('click', () => {
-    radioButtonRef.checked = false;
-    counterGreet.innerHTML = instaOfGreetFactory.resetCounter();
+    for (let i = 0; i < radioButtonRef.length; i++) {
+        radioButtonRef[i].checked = false;
+    }
+    
+    localStorage.clear();
+    location.reload();
 })
